8624d68f4d335d5802e1b0c0850249a84b4686b8
darmun <gamer1305@wp.pl>
2019-06-09 20:05:43 +0200

    listItem refactored to class component

--- src/Components/TaskList/ListItem.js
+++ src/Components/TaskList/ListItem.js
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-import Delete from "./delete.png"
+import Delete from "./delete.png";
 
 const styles = {
   listItem: {
     font: "200 20px Helvetica, Verdana, sans-serif",
     lineHeight: "1.8rem"
-  }
+  },
+  deleteButton: {}
 };
 
-function ListItem(props) {
-  const { classes } = props;
+class ListItem extends Component {
+  state = {
+    taskDone: false
+  };
 
-  return (
-  <li className={classes.listItem}>
-    <input type="checkbox" />
-    {props.children}
-    <img src={Delete} alt="delete"/>
-  </li>
-  );
+  toggleTaskStatus = ({target:{checked}}) => {
+    this.setState({
+      taskDone: checked
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <li className={classes.listItem}>
+        <input type="checkbox" onChange={this.toggleTaskStatus} />
+        {this.props.children}
+        <button className={classes.deleteButton}>
+          <img src={Delete} alt="delete" />
+        </button>
+      </li>
+    );
+  }
 }
 
 ListItem.propTypes = {
